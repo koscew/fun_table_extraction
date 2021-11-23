51551b7e13aee9271e5d47d1c394d052c17e21f5
koscew <koscew@gmail.com>
2021-11-22 21:08:52 -0800

    no message

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,7 +69,7 @@ if photo and b_p == 'Batter':
     st.write(df)
     cols = ' '.join(df.columns)
     df[cols] = df.apply(' '.join, axis=1)
-    st.write(df[cols].append(cols))
+    st.write(df.cols.append(cols, ignore_index=True))
 elif photo and b_p == 'Pichter':
     img = Image.open(photo)
     img = img.resize((2000, 900))
